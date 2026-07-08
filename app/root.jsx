36a445dba04d2eb81b5b41d47ec52e190c6bbbe7
AdityaChopra18 <adityachopra1808@gmail.com>
2026-07-08 12:21:05 +0530

    Add custom Sunrise favicon

--- app/root.jsx
+++ app/root.jsx
@@ -6,6 +6,10 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+export const links = () => [
+  { rel: "icon", href: "/favicon.ico" },
+];
+
 export default function App() {
   return (
     <html>
